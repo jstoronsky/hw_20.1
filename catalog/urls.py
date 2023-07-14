ffa1b537c4918bbeda49f16a664ec6f0da606772
Ivan Storonsky <jstoronsky@gmail.com>
2023-07-14 20:17:03 +1200

    some commentaries

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.views import show_home_page, show_contacts, show_admin
 
-# urlpatterns = [path('', show_admin)]
-urlpatterns = [path('', show_contacts)]
-# urlpatterns = [path('', show_home_page)]
+urlpatterns = [path('', show_admin)]  # отображение админки
+# urlpatterns = [path('', show_contacts)]  # отображение страницы с контактными данными
+# urlpatterns = [path('', show_home_page)]  # главная страница
