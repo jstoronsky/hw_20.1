9551e1753252636de3f7c394f780209d92695362
Ivan Storonsky <jstoronsky@gmail.com>
2023-07-24 20:07:20 +1200

    hw_21.1 task3

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
+from pytils.translit import slugify
 
 from blog.models import Blog
 
@@ -8,21 +9,50 @@ from blog.models import Blog
 # Create your views here.
 class BlogCreateView(CreateView):
     model = Blog
-    fields = ('header', 'link', 'content', 'preview', 'date_when_created')
+    fields = ('header', 'content', 'preview', 'date_when_created', 'is_published')
     success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            slug = form.save()
+            slug.link = slugify(slug.header)
+            slug.save()
+            return super().form_valid(form)
 
 
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
-    fields = ('header', 'link', 'content', 'preview', 'is_published')
+    fields = ('header', 'content', 'preview', 'is_published')
+    # success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            slug = form.save()
+            slug.link = slugify(slug.header)
+            slug.save()
+            return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:article', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
